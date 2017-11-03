7fb674acaee28e7da63b78d21bac7ceb60439d5e
chris-thompson13 <chris@hiassistant.comgit config --global user.email chris@hiassistant.com>
2017-11-03 15:01:23 -0400

    first version

--- sounds/app.rb
+++ sounds/app.rb
@@ -4,3 +4,11 @@ require 'sinatra/reloader'
 get '/' do
   erb :index
 end
+
+get '/sounds/views/content.erb' do
+  erb :content
+end
+
+get '/sounds/views/contact.erb' do
+  erb :contact
+end
--- sounds/public/js/index.js
+++ sounds/public/js/index.js
@@ -1,1 +1,12 @@
 console.log('Hello world');
+
+
+function change(){
+tiny.classList.remove('signUp')
+tiny.classList.add('signUpVisible')
+console.log(tiny.classList)
+
+}
+
+change()
+console.log(tiny.classList)
