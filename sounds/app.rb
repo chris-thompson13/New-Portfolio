80a1556c00fd3b2da7d80390528056545fe75382
chris-thompson13 <chris@hiassistant.comgit config --global user.email chris@hiassistant.com>
2017-11-07 10:05:48 -0500

    go

--- sounds/app.rb
+++ sounds/app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sendgrid-ruby'
+include SendGrid
 
 get '/' do
   erb :index
@@ -11,4 +13,24 @@ end
 
 get '/sounds/views/contact.erb' do
   erb :contact
+end
+
+post '/' do
+
+  require 'sendgrid-ruby'
+include SendGrid
+
+from = Email.new(email: 'test@example.com')
+to = Email.new(email: 'test@example.com')
+subject = 'Sending with SendGrid is Fun'
+content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+mail = Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+puts response.status_code
+puts response.body
+puts response.headers
+  erb :index
+
 end
--- sounds/public/js/index.js
+++ sounds/public/js/index.js
@@ -10,3 +10,16 @@ console.log(tiny.classList)
 
 change()
 console.log(tiny.classList)
+
+console.log("hi")
+$(document).ready(function () {
+    //Disable cut copy paste
+    $('body').bind('cut copy paste', function (e) {
+        e.preventDefault();
+    });
+
+    //Disable mouse right click
+    $("body").on("contextmenu",function(e){
+        return false;
+    });
+});
